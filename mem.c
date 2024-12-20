d07000fb4990c40c3bfc69e3b3740eb444885a41
Erinnom <4uei565o@duck.com>
2024-12-20 15:10:16 +0100

    reshape spect

--- mem.c
+++ mem.c
@@ -1,12 +1,14 @@
 #define MAX_MEMORY_SIZE 1024*1024
 
+
+static unsigned char mem[MAX_MEMORY_SIZE] = {};
+static int i = 0;
+
 /*
 Objective : allocate memory space
 Input : Number of Bytes
 Output : poiter to this allocated memory
 */
-static unsigned char mem[MAX_MEMORY_SIZE] = {};
-static int i = 0;
 void * malloc(long int size){
 
     if (i + size > MAX_MEMORY_SIZE){
