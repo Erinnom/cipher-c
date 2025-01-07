0a76e9f582ab8ff525e4b96ae27d5d75426021ac
Erinnom <4uei565o@duck.com>
2025-01-07 11:23:02 +0100

    add specification to xor_string function

--- xor.h
+++ xor.h
@@ -1,3 +1,8 @@
+/*
+Objective : cypher a text with a key with the XOR method
+Input : data, data length, key, key length
+Output : cyphered text
+*/
 char * xor_string(char *txt, int txt_length,char *key,int key_length){
     char *output = (char *)malloc(txt_length);
     for (int i = 0; i < txt_length; i++){
